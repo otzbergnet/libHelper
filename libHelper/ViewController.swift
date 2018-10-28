cc84c449b5cfb59fbae96bdad7aef768a558b986
Claus Wolf <claus@mactopics.de>
2018-10-28 06:52:43 +0100

    Initial Commit

---
+++ libHelper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  libHelper
+//
+//  Created by Claus Wolf on 28.10.18.
+//  Copyright © 2018 Claus Wolf. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
